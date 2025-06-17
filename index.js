077aa6b48db629c4d8a5550b21ab1dff8a5f69c3
jhseo <maxidea1024@gmail.com>
2025-06-17 15:57:21 +0900

    adjustments

--- index.js
+++ index.js
@@ -3,17 +3,15 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.send('Hello from Render-hosted Express!');
 });
 
-// Apply bodyParser middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 app.post('/webhook', (req, res) => {
   console.log('Webhook received:', req.body);
-
   res.json(req.body);
 });
 
